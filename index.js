1d11c496e256b6304abf24ae7c1719234c434921
Tevin-Mugairwa <tevin.mugairwa@student.moringaschool.com>
2022-08-16 16:38:54 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(message="roller-skate")
+ {
+    return `This Saturday, I want to ${message}!`;
+  }
+  let mondayWork = function(work="go to the office") 
+  {
+    return `This Monday, I will ${work}.`
+  }
+  
+  let wrapAdjective = function(myStyle="*")
+   {
+    return function(description="special") 
+    {
+      return `You are ${myStyle}${description}${myStyle}!`
+    }
+  }
+
+
